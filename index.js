d312f4efdcde93c04292c6081fc6c9863e7a57fe
James Kyburz <james.kyburz@gmail.com>
2015-11-10 11:24:27 +0100

    standard this package

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 module.exports = screenshot
 
-function screenshot(opt, cb) {
-  cb = cb || function() {}
+function screenshot (opt, cb) {
+  cb = cb || function () {}
   var remote = require('' + 'remote') // prevent static analysis like browserify
-  setTimeout(function() {
-    remote.getCurrentWindow().capturePage(function handleCapture(img) {
+  setTimeout(function () {
+    remote.getCurrentWindow().capturePage(function handleCapture (img) {
       remote.require('fs').writeFile(opt.filename, img.toPng(), cb)
-    });
+    })
   }, opt.delay)
 }
