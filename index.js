fd5ebab210780d71929ce60fd300ba1e35086022
James Kyburz <james.kyburz@gmail.com>
2016-05-17 21:14:01 +0200

    allow older electron versions

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ module.exports = screenshot
 
 function screenshot (opt, cb) {
   cb = cb || function () {}
-  var remote = require('' + 'remote') // prevent static analysis like browserify
+  var remote
+  try {
+    remote = require('' + 'electron').remote
+  } catch (e) {
+    remote = require('' + 'remote')
+  }
   setTimeout(function () {
     remote.getCurrentWindow().capturePage(function handleCapture (img) {
       remote.require('fs').writeFile(opt.filename, img.toPng(), cb)
